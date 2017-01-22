673f44785568b142e85c6e2f5588af2b4ad20f7a
Kenny Nguyen <alphakennyn@gmail.com>
2017-01-22 08:27:14 -0500

    's'

--- app.js
+++ app.js
@@ -19,7 +19,9 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, './www/index')));
+app.use(express.static('./www/index'));
+
+//app.use(express.static(path.join(__dirname, './www/index')));
 
 //app.use('/', index);
 // app.use('/users', users);
